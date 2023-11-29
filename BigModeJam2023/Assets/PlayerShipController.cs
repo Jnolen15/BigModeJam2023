acaed9a9a646aa639202d4b54e437d60e1cee394
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2023-11-28 21:33:51 -0800

    Added movement to Ship
    - Ship now moves with WASD
    - Ship now limited to boundaries which can be offset

--- BigModeJam2023/Assets/PlayerShipController.cs
+++ BigModeJam2023/Assets/PlayerShipController.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class PlayerShipController : MonoBehaviour
 {
     // ====================== Refrences / Variables ======================
-    [SerializeField] private float _moveSpeed = 0.1f;
+    [SerializeField] private float _moveSpeed = 0.01f;
+    [SerializeField] private float _gunCoolDown = 0.1f;
+    // Boundries
+    [SerializeField] private float _xLimit = 10;
+    [SerializeField] private float _yLimit = 10;
+    // Offset for play area
+    [SerializeField] private float _xOffset = 0;
+    [SerializeField] private float _yOffset = 0;
+
 
     private GameplayManager _gameplayManager;
 
@@ -13,18 +21,45 @@ public class PlayerShipController : MonoBehaviour
     // ====================== Setup ======================
     void Start()
     {
-        _gameplayManager = GameObject.Find("Gameplay Manager").GetComponent<GameplayManager>();
+        _gameplayManager = GameObject.Find("GameplayManager").GetComponent<GameplayManager>();
     }
 
     // ====================== Function ======================
     void Update()
     {
-        
+        // Movement
+        float speed = _moveSpeed * Time.timeScale; // adjusting for slow-mo
+        Vector3 translation = Vector3.zero;
+        if (Input.GetKey(KeyCode.D)) translation.x += speed; // Right
+        if (Input.GetKey(KeyCode.A)) translation.x -= speed; // Left 
+        if (Input.GetKey(KeyCode.W)) translation.y += speed; // Up
+        if (Input.GetKey(KeyCode.S)) translation.y -= speed; // Down
+        transform.Translate(translation);
+
+        // Applying Boundries
+        Vector3 currentPos = transform.position;
+        float xLim = _xLimit + _xOffset;
+        float yLim = _yLimit + _yOffset;
+        if (transform.position.x > xLim) currentPos.x = xLim;
+        if (transform.position.x < -xLim) currentPos.x = -xLim;
+        if (transform.position.y > yLim) currentPos.y = yLim;
+        if (transform.position.y < -yLim) currentPos.y = -yLim;
+        transform.position = currentPos;
+
     }
 
     public void TakeDamage()
     {
         _gameplayManager.CurrentHealth -= 1;
+    }
+
+    private void ApplyBoundries()
+    {
+
+    }
+    private void MoveShip()
+    {
+        
     }
 
     // ====================== Collisions ======================
