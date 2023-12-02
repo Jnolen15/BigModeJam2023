5c65b3a1cc89fbb0cbcf3f6c89b655d38965a8ab
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2023-12-01 21:04:26 -0800

    Upgrade Collisions

--- BigModeJam2023/Assets/UpgradeScript.cs
+++ BigModeJam2023/Assets/UpgradeScript.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerProjectileScript : MonoBehaviour
+public class UpgradeScript : MonoBehaviour
 {
     // ====================== Refrences / Variables ======================
-    [SerializeField] private float _range = 20;
-
-    private float _speed = 0.001f;
+    [SerializeField] private float _range = 10;
+    [SerializeField] private float _speed =  -0.003f;
+    [SerializeField] private float _drift = -0.001f;
     private float _yPosition;
     private float _distanceTraveled = 0;
 
@@ -15,28 +15,19 @@ public class PlayerProjectileScript : MonoBehaviour
     void Start()
     {
         _yPosition = transform.position.y;
+        _drift = Random.Range(-10, 10) / 10000f;
     }
 
     // ====================== Function ======================
 
     void Update()
     {
-        transform.Translate(new Vector3(0, _speed * Time.timeScale, 0));
+        transform.Translate(new Vector3(_drift * Time.timeScale, _speed * Time.timeScale, 0));
 
         _distanceTraveled += transform.position.y - _yPosition;
         _yPosition = transform.position.y;
         if (_distanceTraveled > _range) // destroying
             Destroy(gameObject);
 
-    }
-
-    public void SetSpeed(float speed)
-    {
-        _speed = speed;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Player shot: " + collision.name);   
     }
 }
